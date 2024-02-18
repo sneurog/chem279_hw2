0c993975ed2650e2e2fabe790485c483534c7f2c
Sarah Gomez <sarah_gomezz@berkeley.edu>
2024-02-17 20:47:05 -0500

    add api for file reader section 2

--- readfile.h
+++ readfile.h
@@ -1,21 +1,30 @@
-// Header file to read in the automic coordinates 
+//readfile.h
 #ifndef READ_FILE
 #define READ_FILE
 
-#include <iostream>
+#include <vector>
+#include <tuple>
 
-
-
-struct Atom_1d
-{
-
-double x_w1, w1, x_angular_w1;
-double x_w2, w2, x_angular_w2;
+// Structure for 1D atom data
+struct Atom_1d {
+    double x_w1, w1, x_angular_w1;
+    double x_w2, w2, x_angular_w2;
 };
 
 
 
-Atom_1d read_atoms_1d(const std::string& filename1d); 
+struct Overlap {
+    double x1, y1, z1, exponent1;
+    int angular_momentum1;
+
+    double x2, y2, z2, exponent2;
+    int angular_momentum2;
+};
 
 
-#endif //READ_FILE
+
+// Function declarations
+Atom_1d read_atoms_1d(const std::string& filename1d);
+Overlap read_overlap(const std::string& filename);
+
+#endif // READ_FILE
